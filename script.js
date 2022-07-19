45368e3867ade49a78226807ac27813bbed9e952
Jonathan Andreas Pangkey <102292312+Jonathanpangkey@users.noreply.github.com>
2022-07-19 10:06:01 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let weather = {
-	apiKey: "Your Api key",
+	apiKey: "98d6c48880133a6cb94dbec684ab7cc8",
 	fetchWeather: function (city) {
 		fetch(
 			"https://api.openweathermap.org/data/2.5/weather?q=" +
